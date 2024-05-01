a76ed06029566e97d3db9bf8392509d1f6b9fac3
dlwhsk0 <hana.dev.314@gmail.com>
2024-05-02 01:37:12 +0900

    feat: UserController, StoryController 결과 처리 수정

--- src/main/java/com/nextpage/backend/controller/UserController.java
+++ src/main/java/com/nextpage/backend/controller/UserController.java
@@ -2,17 +2,18 @@ package com.nextpage.backend.controller;
 
 import com.nextpage.backend.config.jwt.TokenService;
 import com.nextpage.backend.dto.request.UserCreateRequest;
-import com.nextpage.backend.dto.response.ApiResponse;
 import com.nextpage.backend.dto.response.SignUpResponseDTO;
 import com.nextpage.backend.dto.response.UserResponseDTO;
+import com.nextpage.backend.result.ResultResponse;
 import com.nextpage.backend.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static com.nextpage.backend.result.ResultCode.*;
 
 @Tag(name = "Users", description = "User 관리")
 @RestController
@@ -28,34 +29,30 @@ public class UserController {
 
     @Operation(summary = "회원가입", description = "유저를 생성합니다.")
     @PostMapping
-    public ResponseEntity<ApiResponse> createUser(@RequestBody @Valid UserCreateRequest request) {
+    public ResponseEntity<ResultResponse> createUser(@RequestBody @Valid UserCreateRequest request) {
         SignUpResponseDTO user = userService.createUser(request);
-        return ResponseEntity.ok()
-                .body(new ApiResponse(HttpStatus.OK.value(), "유저 정보를 불러왔습니다.", user));
+        return ResponseEntity.ok(ResultResponse.of(USER_CREATE_SUCCESS, user));
     }
 
     @Operation(summary = "회원탈퇴", description = "유저를 삭제합니다. (Hard Delete)")
     @DeleteMapping("/delete")
-    public ResponseEntity<ApiResponse> deleteUser(HttpServletRequest request) {
+    public ResponseEntity<ResultResponse> deleteUser(HttpServletRequest request) {
         userService.deleteUser(request);
-        return ResponseEntity.ok()
-                .body(new ApiResponse(HttpStatus.OK.value(), "유저 정보를 삭제했습니다.", null));
+        return ResponseEntity.ok(ResultResponse.of(USER_DELETE_SUCCESS, null));
     }
 
     @Operation(summary = "액세스 토큰 재발급", description = "리프레쉬 토큰을 검사해 액세스 토큰 재발급 요청을 처리합니다.")
     @PostMapping("/auth/token")
-    public ResponseEntity<ApiResponse> reGenerateAccessToken(HttpServletRequest request) {
+    public ResponseEntity<ResultResponse> reGenerateAccessToken(HttpServletRequest request) {
         // 이 때 받은 토큰은 refresh 토큰
         String accessToken = tokenService.reGenerateAccessToken(request);
-        return ResponseEntity.ok()
-                .body(new ApiResponse(HttpStatus.OK.value(), "새로운 액세스 토큰 발급 완료", accessToken));
+        return ResponseEntity.ok(ResultResponse.of(TOKEN_CREATE_SUCCESS, accessToken));
     }
 
     @Operation(summary = "토큰으로 사용자 조회 API", description = "토큰을 통해 사용자 정보를 조회합니다.")
     @GetMapping("/details") // 토큰으로 사용자 조회
-    public ResponseEntity<ApiResponse> getUserByToken(HttpServletRequest request) {
+    public ResponseEntity<ResultResponse> getUserByToken(HttpServletRequest request) {
         UserResponseDTO userResponseDTO = userService.getUserInfo(request);
-        return ResponseEntity.ok()
-                .body(new ApiResponse(HttpStatus.OK.value(), "조회 완료." , userResponseDTO));
+        return ResponseEntity.ok(ResultResponse.of(USER_DETAIL_INFO_SUCCESS, userResponseDTO));
     }
 }
--- src/main/java/com/nextpage/backend/result/ResultCode.java
+++ src/main/java/com/nextpage/backend/result/ResultCode.java
@@ -8,11 +8,17 @@ import lombok.Getter;
 public enum ResultCode {
 
     // user
-    USER_REGISTRATION_SUCCESS("U001", "사용자 등록 성공"),
-    USER_USERNAME_DUPLICATED("U002", "회원 아이디 중복"),
-    USER_USERNAME_NOT_DUPLICATED("U003", "회원 아이디 중복되지 않음"),
-    USER_LOGIN_SUCCESS("U004", "회원 로그인 성공"),
-    USER_LOGOUT_SUCCESS("U005", "회원 로그아웃 성공"),
+    USER_CREATE_SUCCESS("U001", "회원 생성 성공"),
+    USER_DELETE_SUCCESS("U002", "회원 탈퇴 성공"),
+    USER_DETAIL_INFO_SUCCESS("U003", "회원 상세 조회 성공"),
+    USER_UPDATE_SUCCESS("U004", "회원 상세정보 수정 성공"),
+
+    // AUTH
+    TOKEN_CREATE_SUCCESS("A001", "토큰 생성 성공"),
+
+    // STORY
+    STORY_LIST_SUCCESS("S001", "스토리 목록 조회 성공"),
+    STORY_DETAIL_INFO_SUCCESS("S002", "스토리 상세 조회 성공"),
     ;
 
     private final String code;
