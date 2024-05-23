de1f2af517cbebe8a1b1c93fa5887584b3df3bfc
kanguk <lm0828@naver.com>
2024-05-23 13:28:31 +0900

    test : getRootStories test 작성
    getRootStories 성공 , 스토리 존재하지 않음 테스트 작성

---
+++ src/test/java/com/nextpage/backend/service/StoryServiceTest.java
@@ -0,0 +1,97 @@
+package com.nextpage.backend.service;
+
+import com.nextpage.backend.config.jwt.TokenService;
+import com.nextpage.backend.dto.response.RootResponseDTO;
+import com.nextpage.backend.dto.response.StoryDetailsResponseDTO;
+import com.nextpage.backend.entity.Story;
+import com.nextpage.backend.error.exception.story.StoryNotFoundException;
+import com.nextpage.backend.repository.StoryRepository;
+import com.nextpage.backend.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class StoryServiceTest {
+    @InjectMocks
+    private StoryService storyService;
+    @Mock
+    private StoryRepository storyRepository;
+
+    @Mock
+    private ImageService imageService;
+
+    @Mock
+    private TokenService tokenService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private HttpServletRequest request;
+
+    private Story story;
+    private Story parentStory;
+
+    @BeforeEach
+    void setUp() {
+        parentStory = Story.builder()
+                .id(2L)
+                .content("Parent content")
+                .imageUrl("ParentImageUrl")
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .isDeleted(false)
+                .userNickname("parentNickname")
+                .parentId(null)
+                .build();
+
+        story = Story.builder()
+                .id(1L)
+                .content("Content")
+                .imageUrl("ImageUrl")
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .isDeleted(false)
+                .userNickname("testNickname")
+                .parentId(parentStory)
+                .build();
+    }
+
+    @DisplayName("루트 스토리 조회 -> 성공")
+    @Test
+    void getRootStories_성공(){
+        when(storyRepository.findRootStories()).thenReturn(Arrays.asList(story));
+
+        List<RootResponseDTO> rootStories = storyService.getRootStories();
+
+        assertThat(rootStories).isNotEmpty();
+        assertThat(rootStories.get(0).getId()).isEqualTo(story.getId());
+        verify(storyRepository, times(1)).findRootStories();
+    }
+
+    @DisplayName("루트 스토리 조회 -> 스토리가 존재하지 않음")
+    @Test
+    void getRootStories_스토리가_존재하지_않음() {
+        when(storyRepository.findRootStories()).thenReturn(Collections.emptyList());
+
+        assertThrows(StoryNotFoundException.class, () -> storyService.getRootStories());
+    }
+
+
+}
