5e224b461724f630802f70d576aeb4aa8a33b468
kanguk <lm0828@naver.com>
2024-06-07 10:35:09 +0900

    refactor : Bookmark 엔티티 수정
    createAt, isDeleted 필드 추가

--- src/main/java/com/nextpage/backend/entity/Bookmark.java
+++ src/main/java/com/nextpage/backend/entity/Bookmark.java
@@ -3,6 +3,7 @@ package com.nextpage.backend.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -23,12 +24,20 @@ public class Bookmark {
     @Column(name = "\"storyId\"", nullable = false)
     private Long storyId;
 
+    @Column(name = "\"createdAt\"", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "\"isDeleted\"", nullable = false)
+    private boolean isDeleted;
+
     protected Bookmark() {
     }
 
     public Bookmark(User user, Long storyId) {
         this.user = user;
         this.storyId = storyId;
+        this.createdAt = LocalDateTime.now();
+        this.isDeleted = false;
     }
 
     public void addBookmark(User user, Bookmark bookmark) {
